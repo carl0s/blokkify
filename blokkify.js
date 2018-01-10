7ac0a574bd92f4644ed46e74ee66c15089590c22
carlo frinolli <carlo.frinolli@nois3.it>
2018-01-10 17:41:09 +0100

    fixed css but not loading fonts

--- blokkify.js
+++ blokkify.js
@@ -1,11 +1,18 @@
-var fromRep = document.querySelectorAll('.from-rep');
-var index = 0, length = fromRep.length;
-for ( ; index < length; index++) {
-    fromRep[index].style.display = "none";
-}
+// var fromRep = document.querySelectorAll('.from-rep');
+// var index = 0, length = fromRep.length;
+// for ( ; index < length; index++) {
+//     fromRep[index].style.display = "none";
+// }
+//
+// var fromRepLinks = document.querySelectorAll('.rep-small');
+// var index = 0, length = fromRepLinks.length;
+// for ( ; index < length; index++) {
+//     fromRepLinks[index].style.display = "none";
+// }
 
-var fromRepLinks = document.querySelectorAll('.rep-small');
-var index = 0, length = fromRepLinks.length;
-for ( ; index < length; index++) {
-    fromRepLinks[index].style.display = "none";
+var all = document.getElementsByTagName("*");
+
+for (var i=0, max=all.length; i < max; i++) {
+     // Do something with the element here
+  all[i].style.fontFamily = "BLOKK";
 }
